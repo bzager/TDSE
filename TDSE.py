161bd820de71faa1ca98d1cb0a1c067b9bb5b91d
Ben Zager <bzager47@gmail.com>
2017-04-09 15:16:48 -0400

    added Crank Nicolson

--- TDSE.py
+++ TDSE.py
@@ -13,35 +13,37 @@ import time
 
 # constants
 L = 1.0 # length of domain [-L/2, L/2]
-T = 1.0 # total time
+T = 0.005 # total time
 
-nx = 200 # spatial grid size
-nt = 100 # time grid size
+nx = 400 # spatial grid size
+nt = 800 # time grid size
 
-x = np.linspace(-L/2,L/2,nx) # spatial gri
+x = np.linspace(-L/2,L/2,nx) # spatial grid
 t = np.linspace(0,T,nt) # time grid
 
 dx = x[1] - x[0] # space step size
 dt = t[1] - t[0] # time step size
 
-a = (dt/dx**2)*1.0j # 
+a = 1/dx**2 # 
 
 # potential function
 def V(x):
 	#U = 0 	# free particle
 	#U = harmonic(x)
-	U = well(x)
+	#U = well(x)
 	#U = triangle(x)
+	#U = step(x)
+	U = barrier(x)
 	return U
 
 # harmonic potential
-def harmonic(x):
-	return np.square(x)
+def harmonic(x,h=100000):
+	return h*np.square(x)
 
 # potential well
 # args: position x, left bound of well l, right bound of well r, height h
 # default height is infinite well
-def well(x,l=-0.2*L,r=0.2*L,h=10000):
+def well(x,l=-0.1*L,r=0.1*L,h=10000000):
 	if x < l or x > r:
 		U = h
 	else:
@@ -52,21 +54,26 @@ def well(x,l=-0.2*L,r=0.2*L,h=10000):
 def triangle(x):
 	return np.abs(x)
 
-# Animate solution
-def animate(data):
-    x = np.linspace(-L/2,L/2,nx)
-    y = data
-    real.set_data(x,np.real(y))
-    imag.set_data(x,np.imag(y))
-    abso.set_data(x,np.absolute(y))
-    return real,imag,abso
+# potential step
+def step(x,pos=0.3*L,h=10000000):
+	if x > pos:
+		return h
+	else:
+		return 0
+
+# potential barrier
+def barrier(x,l=0.1*L,r=0.2*L,h=10000000):
+	if x > l and x < r:
+		return h
+	else:
+		return 0
 
 # returns ith diagonal element of Hamiltonian matrix 
 def diagonal(i,x):
 	return 1 - 2*a + a*(dx**2)*V(x[i])
 
 # constructs Hamiltonian matrix
-def constructH(x,nx,nt):
+def constructH():
 	off = a*np.ones((nx-1,),dtype=complex)
 	dia = np.asarray([diagonal(i,x) for i in range(nx)])
 	H = np.diag(dia) + np.diag(off,k=1) + np.diag(off,k=-1)
@@ -76,6 +83,14 @@ def constructH(x,nx,nt):
 
 	return H
 
+def constructA(H):
+	A = np.eye(nx) + 1j*dt*H
+	return A
+
+def constructB(H):
+	B = np.eye(nx) - 1j*dt*H
+	return A
+
 # turns a matrix into a sparse matrix
 def sparsify(A):
 	return csc_matrix(A)
@@ -83,41 +98,81 @@ def sparsify(A):
 # initializes psi with initial condition
 # and zeros for the rest 
 # row n is solution on whole domain at time n
-def initPsi(nx,nt,x,A,k,sig):
+def initPsi(x0,A,k,sig):
 	psi = np.zeros([nt,nx],dtype=complex)
-	psi[0,:] = packet(x,A,k,sig)
+	psi[0,:] = packet(x0,A,k,sig)
 
 	return psi
 
-def packet(x,A,k,sig):
-	return A*np.exp(-(x)**2/(4*sig**2) + k*x*1.0j)
+def packet(x0,A,k,sig):
+	return A*np.exp(-(x-x0)**2/(2*sig**2) - k*x*1j)
 
-# Backward-Time Centered-Space finite difference scheme
-def BTCS(x,nx,nt,A,k,sig):
-
-	H = constructH(x,nx,nt)
-	H = sparsify(H)
-	psi = initPsi(nx,nt,x,A,k,sig)
-
-	#times.append(time.time())
+def BTCS(A,B,psi):
 
 	for n in range(nt-1):
 		#psi[n+1,:] = np.linalg.solve(H,psi[n,:]) # non sparse solver
-		psi[n+1,:] = linalg.spsolve(H,psi[n,:]) # sparse solver
-		psi[n+1,-1] = 0.5*(psi[n+1,1] + psi[n+1,-2])
-
-	#times.append(time.time())
+		psi[n+1,:] = linalg.spsolve(A,psi[n,:]) # sparse solver
 
 	return psi
 
-A = 0.5 # amplitude
-k = 2 # wave number
-sig = 0.02 # width of wave packet
+
+def CrankNicolson(A,B,psi):
+	for n in range(nt-1):
+		#psi[n+1,:] = np.linalg.solve(H,psi[n,:]) # non sparse solver
+		b = B*psi[n,:]
+		psi[n+1,:] = linalg.spsolve(A,b) # sparse solver
+
+	return psi
+
+
+# Backward-Time Centered-Space finite difference scheme
+# 
+def solver(nx,nt,x,x0,A,k,sig,method):
+
+	H = constructH()
+	A = constructA(H)
+	B = constructB(H)
+	A = sparsify(A)
+	psi = initPsi(x0,A,k,sig)
+
+	psi = method(A,B,psi)
+	
+	return psi
+
+x0 = -0.2
+A = 1 # amplitude
+k = 100 # wave number
+sig = 0.1 # width of wave packet
 
 times = []
-psi = BTCS(x,nx,nt,A,k,sig)
 
+method = CrankNicolson
+#method = BTCS
+
+psi = solver(nx,nt,x,x0,A,k,sig,method)
 U = np.asarray([V(x[i]) for i in range(nx)])
+
+
+# Animate solution
+def animHelper(data):
+    y = data
+    real.set_data(x,np.real(y))
+    imag.set_data(x,np.imag(y))
+    abso.set_data(x,np.absolute(y))
+    return real,imag,abso
+
+def animate(psi):
+	fig = figure()
+	ax = plt.axes(xlim=(-L/2,L/2),ylim=(-A,A))
+	real, = ax.plot([],[],lw=2,color='b',label='Real Part')
+	imag, = ax.plot([],[],lw=2,color='r',label='Imaginary Part')
+	abso, = ax.plot([],[],lw=2,color='k',label='Absolute Value')
+	potent = ax.plot(x,U,color='0.6',label='V(x)')
+	plt.legend()
+
+	anim = animation.FuncAnimation(fig,animHelper,frames=psi,interval=1)
+
+	return anim
 
 fig = plt.figure(1)
 plt.plot(x,np.absolute(psi[0,:]),color='k',label='Absolute Value')
@@ -125,19 +180,12 @@ plt.plot(x,np.real(psi[0,:]),color='b',label='Real Part')
 plt.plot(x,np.imag(psi[0,:]),color='r',label='Imaginary Part')
 plt.plot(x,U,color='0.8',label='Potential')
 plt.xlim([-L/2,L/2])
-plt.ylim(-1,1)
+plt.ylim(-A,A)
 plt.title("Initial Condition")
 plt.legend()
 
-fig = plt.figure(2)
-ax = plt.axes(xlim=(-L/2,L/2),ylim=(-1,1))
-real, = ax.plot([],[],lw=2,color='b',label='Real Part')
-imag, = ax.plot([],[],lw=2,color='r',label='Imaginary Part')
-abso, = ax.plot([],[],lw=2,color='k',label='Absolute Value')
-potent = ax.plot(x,U,color='0.6',label='V(x)')
-plt.legend()
 
-anim = animation.FuncAnimation(fig,animate,frames=psi,interval=100)
+anim = animation.FuncAnimation(fig,anim,frames=psi,interval=1)
 
 #times.append(time.time())
 #timeDif = np.diff(times)
